6183d6ad9f81a7f7231adbbe1d16125808874440
g16933os <g16933os@s30401.tsuda.ac.jp>
2018-04-19 13:43:02 +0900

    更新した

--- src/Dai2.java
+++ src/Dai2.java
@@ -3,7 +3,8 @@ public class Dai2 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello,World!");
-		
+		System.out.println("4/19(Thr)第2回の授業です");
+		System.out.println("更新したいときはどうするのだ？？");
 	}
 
 }
